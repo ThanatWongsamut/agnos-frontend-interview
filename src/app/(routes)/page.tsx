fbb9e2d9318d6211e64464bb9c85732949f8de25
ThanatWongsamut <thanat.wongsamut@gmail.com>
2024-01-20 18:39:46 +0700

    feat(frontend): :zap: Add image preload

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -2,7 +2,7 @@
 
 import { FC, useState } from 'react';
 
-import Image from 'next/image';
+import Head from 'next/head';
 
 import Card from '@components/common/card';
 import Button from '@components/common/button';
@@ -10,6 +10,7 @@ import Question from '@components/question/question';
 
 import AbsConfig from '@assets/config/abs-config.json';
 import HandConfig from '@assets/config/hand-config.json';
+import { Mapping } from '@/utils/types';
 
 const Page: FC = () => {
   const [questionState, setQuestionState] = useState<string>('abs');
@@ -21,7 +22,10 @@ const Page: FC = () => {
   };
 
   const nextHandler = () => {
-    setQuestionState('finger');
+    // Make sure the answer is not empty
+    if (absAnswer != '') {
+      setQuestionState('finger');
+    }
   };
 
   const fingerChangeHandler = (key: string) => {
@@ -33,41 +37,61 @@ const Page: FC = () => {
   };
 
   return (
-    <main className="flex h-screen flex-col items-center justify-center bg-white">
-      <div className="flex flex-col gap-4 w-full px-6 sm:w-[640px] md:w-[768px] mx-auto justify-center items-center">
-        <Card className="h-full">
-          {questionState == 'abs' && (
-            <Question
-              header="จุดไหนที่คุณปวดท้องมากที่สุด ?"
-              config={AbsConfig}
-              onChange={absChangeHandler}
-            />
-          )}
+    <>
+      <main className="flex h-screen flex-col items-center justify-center bg-white">
+        <Head>
+          <link rel="preload" href={`/image/${AbsConfig.default}`} as="image" />
 
-          {questionState == 'finger' && (
-            <Question
-              header="จุดไหนที่คุณปวดนิ้วมากที่สุด ?"
-              config={HandConfig}
-              onChange={fingerChangeHandler}
-            />
-          )}
-        </Card>
+          {AbsConfig.mapping.map((shape: Mapping) => {
+            return (
+              <link key={shape.name} rel="preload" href={`/image/${shape.name}.png`} as="image" />
+            );
+          })}
 
-        <>
-          {questionState == 'abs' && (
-            <Button full disabled={absAnswer == ''} onClick={nextHandler}>
-              ต่อไป
-            </Button>
-          )}
+          <link rel="preload" href={`/image/${HandConfig.default}`} as="image" />
 
-          {questionState == 'finger' && (
-            <Button full disabled={fingerAnswer == ''} onClick={submitHandler}>
-              ยืนยัน
-            </Button>
-          )}
-        </>
-      </div>
-    </main>
+          {HandConfig.mapping.map((shape: Mapping) => {
+            return (
+              <link key={shape.name} rel="preload" href={`/image/${shape.name}.png`} as="image" />
+            );
+          })}
+        </Head>
+
+        <div className="flex flex-col gap-4 w-full px-6 sm:w-[640px] md:w-[768px] mx-auto justify-center items-center">
+          <Card className="h-full">
+            {questionState == 'abs' && (
+              <Question
+                header="จุดไหนที่คุณปวดท้องมากที่สุด ?"
+                config={AbsConfig}
+                onChange={absChangeHandler}
+              />
+            )}
+
+            {questionState == 'finger' && (
+              <Question
+                header="จุดไหนที่คุณปวดนิ้วมากที่สุด ?"
+                config={HandConfig}
+                onChange={fingerChangeHandler}
+              />
+            )}
+          </Card>
+
+          <>
+            {questionState == 'abs' && (
+              <Button full disabled={absAnswer == ''} onClick={nextHandler}>
+                ต่อไป
+              </Button>
+            )}
+
+            {questionState == 'finger' && (
+              <Button full disabled={fingerAnswer == ''} onClick={submitHandler}>
+                ยืนยัน
+              </Button>
+            )}
+          </>
+        </div>
+      </main>
+    </>
   );
 };
 
